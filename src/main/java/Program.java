3bcdc98bbf0aa11008e60d8f065456f351e13be7
koxx4 <piotr815@poczta.fm>
2021-05-28 23:21:07 +0200

    uh

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -2,6 +2,6 @@ public class Program
 {
     public static void main(String[] args)
     {
-        System.out.println("Mateusz wyslal .class zamiast .java\n l00rem ipsum gipsum");
+        System.out.println("l00rem ipsum gipsum");
     }
 }
